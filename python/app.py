bf0d1dbf9e420393e5ad0ca59e3a68617d7f331f
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-08-05 20:48:16 +0530

    python appliaction

--- python/app.py
+++ python/app.py
@@ -1,4 +1,13 @@
 # this is a simple python application
 # general app
 
-print("Hello, World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Pranay! Your Python app is up and running."
+
+if __name__ == "__main__":
+    app.run(debug=True)
