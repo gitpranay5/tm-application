835f3c0db880aafcf197c2767d7583257751b685
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-08-06 16:10:47 +0530

    new commit

--- java/src/main/java/com/example/App.java
+++ java/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Pranay! Your Maven project is alive..!");
+        System.out.println("Hello, Pranay! Your Maven project is alive..!..");
     }
 }
