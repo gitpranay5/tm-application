c998e7a1b14b74bdc4d0aafc27edffc2af8ad654
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-08-08 10:45:15 +0530

    updates

--- java/src/main/java/com/example/App.java
+++ java/src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hi, Pranay! Your Maven project is alive..! inside the feature branch");
+        System.out.println("Hi.., Pranay! Your Maven project is alive..! inside the feature branch");
     }
 }
