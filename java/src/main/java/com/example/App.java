608ad2e26b1612c78fe653e8ba3239536652938b
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-08-08 17:01:10 +0530

    folder renamed

--- java/src/main/java/com/example/App.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hi.., Pranay! Your Maven project is alive..! inside the feature branch");
-    }
-}
