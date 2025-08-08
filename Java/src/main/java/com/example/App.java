cbfe68a065ce9e3db4ceaf635ad2ab6f54c425bf
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-08-08 16:57:08 +0530

    new changes in folder & other things

---
+++ Java/src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hi.., Pranay! Your Maven project is alive..! inside the feature branch");
+    }
+}
