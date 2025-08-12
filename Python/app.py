36fc8d76aa7e90a0dbd4d982ca81269bd717d760
Pranay Jujjuri <pranayjujjuri@gmail.com>
2025-08-12 10:10:18 +0530

    main updates

---
+++ Python/app.py
@@ -0,0 +1,13 @@
+# this is a simple python application
+# general app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Pranay! Your Python app is up and running..."
+
+if __name__ == "__main__":
+    app.run(debug=True)
